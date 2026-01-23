cad38af9060a70ea464110033578fda52ead3635
Gdev <125574259+gisachris@users.noreply.github.com>
2026-01-23 08:46:53 +0200

    feat: create booking builder for defaults and easy testing

--- migrations/1769073917674_create-bookings-table.ts
+++ migrations/1769073917674_create-bookings-table.ts
@@ -1,35 +1,34 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate'
 
-export const shorthands: ColumnDefinitions | undefined = undefined;
+export const shorthands: ColumnDefinitions | undefined = undefined
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-    pgm.createTable('bookings', {
-        id: {
-            type: 'serial',
-            primaryKey: true,
-        },
-        car_id: {
-            type: 'integer',
-            references: 'cars',
-            onDelete: 'CASCADE'
-        },
-        renter_id: {
-            type: 'integer',
-            references: 'users',
-            onDelete: 'CASCADE'
-        },
-        state: {
-            type: 'text'
-        },
-        start_date: {
-            type: 'timestamptz'
-        },
-        end_date: {
-            type: 'timestamptz'
-        }
-    })
+  pgm.createTable('bookings', {
+    id: {
+      type: 'serial',
+      primaryKey: true,
+    },
+    car_id: {
+      type: 'integer',
+      references: 'cars',
+      onDelete: 'CASCADE',
+    },
+    renter_id: {
+      type: 'integer',
+      references: 'users',
+      onDelete: 'CASCADE',
+    },
+    state: {
+      type: 'text',
+    },
+    start_date: {
+      type: 'timestamptz',
+    },
+    end_date: {
+      type: 'timestamptz',
+    },
+  })
 }
 
-export async function down(pgm: MigrationBuilder): Promise<void> {
-}
+export async function down(pgm: MigrationBuilder): Promise<void> {}
---
+++ src/application/booking/booking.builder.ts
@@ -0,0 +1,60 @@
+import { type Except } from 'type-fest'
+
+import { type CarID } from '../car'
+import { type UserID } from '../user'
+
+import {Booking, type BookingID, type BookingProperties} from './booking'
+import { BookingState } from './booking-state'
+
+export class BookingBuilder {
+    private data: BookingProperties = {
+        id: 1 as BookingID,
+        carId: 1 as CarID,
+        renterId: 1 as UserID,
+        state: BookingState.PENDING,
+        startDate: new Date('2023-08-08T14:07:27')
+    }
+
+    public withId(id: BookingID): this {
+    this.data.id = id
+    return this
+  }
+
+  public withCarId(carId: CarID): this {
+    this.data.carId = carId
+    return this
+  }
+
+  public withRenterId(renterId: UserID): this {
+    this.data.renterId = renterId
+    return this
+  }
+
+  public withState(state: BookingState): this {
+    this.data.state = state
+    return this
+  }
+
+  public withStartDate(startDate: Date): this {
+    this.data.startDate = startDate
+    return this
+  }
+
+  public withEndDate(endDate: Date): this {
+    this.data.endDate = endDate
+    return this
+  }
+
+  public build(): Booking {
+    return new Booking(this.data)
+  }
+
+  public buildProperties(): BookingProperties {
+    return { ...this.data }
+  }
+
+  public buildCreateData(): Except<BookingProperties, 'id'> {
+    const { id, ...createData } = this.data
+    return createData
+  }
+}
